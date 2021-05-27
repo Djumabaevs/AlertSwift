465fd883003918a990d898558542b6ee98762b55
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-27 21:05:18 +0600

    Initial Commit

---
+++ AlertSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertSwift
+//
+//  Created by Bakyt Dzhumabaev on 27/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
