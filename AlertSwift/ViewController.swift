87b7e0092ce70d7ceb50609cc3cff7a202d29f63
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-27 21:22:44 +0600

    drag views to view swift code

--- AlertSwift/ViewController.swift
+++ AlertSwift/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordAgainText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var usernameText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signupClicked(_ sender: Any) {
+    }
+    
 }
 
